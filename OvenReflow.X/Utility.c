8278b8d50c7867e05a68b79bbf93fae4cd6e29e3
fromo <f_romo_rivera@outlook.com>
2025-01-05 10:17:05 -0500

    The Utility file is created for general functions that may be necessary in the project. At the moment it contains the function that obtains the absolute value of two unsigned 32-bit numbers.

---
+++ OvenReflow.X/Utility.c
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ CLOCK PLIB
+
+  Company:
+    Sicoy Ltda.
+
+  File Name:
+    Utility.c
+
+  Summary:
+    File of useful functions in the project
+
+  Description:
+    None
+ * 
+ * Author: Fabian Romo
+
+*******************************************************************************/
+#include "definitions.h"
+
+//Gets the unsigned 32-bit absolute value
+uint32_t abs_diff_uint32(uint32_t a, uint32_t b)
+{
+    return (a > b)? (a -b):(b-a);
+} 
