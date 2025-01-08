443563c4a7af1c3755ad3fd063a64ab5e6250699
fromo <f_romo_rivera@outlook.com>
2025-01-08 15:52:32 -0500

    In the GLCD task, the entire process to initialize and clear the LCD is performed.
    A function is also created to inform anyone that the task is idle and ready to process the request for a graphic or printout on the LCD.

--- src/appglcd.c
+++ src/appglcd.c
@@ -34,7 +34,7 @@
 // Section: Global Data Definitions
 // *****************************************************************************
 // *****************************************************************************
-
+#define LCD_START_LINE_ADDR	(66-2)
 // *****************************************************************************
 /* Application Data
 
@@ -51,14 +51,24 @@
 */
 
 APPGLCD_DATA appglcdData;
-
+const uint8_t commandsInitializeGLCD[0x08] = 
+{
+    0x21, // 0 LCD Extended Commands.
+    0xC8, // 1 Set LCD Vop (Contrast). 0xC8
+    0x04 | !!(LCD_START_LINE_ADDR & (1u << 6)),  // 2 Set Temp S6 for start line
+    0x40 | (LCD_START_LINE_ADDR & ((1u << 6) - 1)), // 3 Set Temp S[5:0] for start line
+    0x12, // 4 LCD bias mode 1:68.
+    0x20, // 5 LCD Standard Commands, Horizontal addressing mode.
+    0x08, // 6 LCD blank
+    0x0C  // 7 LCD in normal mode
+};
 // *****************************************************************************
 // *****************************************************************************
 // Section: Application Callback Functions
 // *****************************************************************************
 // *****************************************************************************
 extern uint32_t abs_diff_uint32(uint32_t a, uint32_t b);
-extern bool IsGLCDTaskIdle (void);
+extern bool IsSPI1TaskIdle (void);
 extern void LCDSend(unsigned char data, unsigned char cd);
 /* TODO:  Add any necessary callback functions.
 */
@@ -69,6 +79,14 @@ extern void LCDSend(unsigned char data, unsigned char cd);
 // *****************************************************************************
 // *****************************************************************************
 
+bool IsGLCDTaskIdle (void)
+{
+    if (APPGLCD_STATE_IDLE == appglcdData.state)
+    {
+        return true;
+    }
+    return false;
+}
 
 /* TODO:  Add any necessary local functions.
 */
@@ -127,34 +145,94 @@ void APPGLCD_Tasks ( void )
         {
             if ( abs_diff_uint32(RTC_Timer32CounterGet(), appglcdData.adelay) > _1000ms)
             {
-                appglcdData.state = APPGLCD_STATE_LCD_EXTENDED_COMMANDS;
+                appglcdData.pointerX = 0x00;
+                appglcdData.state = APPGLCD_STATE_LCD_COMMANDS;
             }
             break;
         }
         /* TODO: implement your application state machine.*/
-        case APPGLCD_STATE_LCD_EXTENDED_COMMANDS:
+        case APPGLCD_STATE_LCD_COMMANDS:
         {
-            if (IsGLCDTaskIdle ()) //I wait until the SERCOM1 task is idle
+            if (IsSPI1TaskIdle ()) //I wait until the SERCOM1 task is idle
             {
-                LCDSend(0x21, SEND_CMD);
-                appglcdData.state = APPGLCD_STATE_LCD_SET_VOP;
+                LCDSend(commandsInitializeGLCD[appglcdData.pointerX], SEND_CMD);
+                appglcdData.pointerX++;
+                if (appglcdData.pointerX >= sizeof(commandsInitializeGLCD))
+                {    
+                    appglcdData.state = APPGLCD_STATE_START_CLEANING_GLCD;
+                }    
             }
             break;
         }
-        case APPGLCD_STATE_LCD_SET_VOP:
+        case APPGLCD_STATE_START_CLEANING_GLCD:
         {
-            if (IsGLCDTaskIdle ()) //I wait until the SERCOM1 task is idle
+            if (IsSPI1TaskIdle ()) //I wait until the SERCOM1 task is idle
             {
-                
+                appglcdData.pointerX = 0x00;
+                appglcdData.state = APPGLCD_STATE_CLEAR_LCD;
             }
             break;
-        }    
+        }
+        case APPGLCD_STATE_CLEAR_LCD:
+        {
+            if (appglcdData.pointerX < sizeof(appglcdData.LcdMemory))
+            {
+                appglcdData.LcdMemory[appglcdData.pointerX] = 0x00;
+                appglcdData.pointerX++;
+            }
+            else
+            {
+                appglcdData.pointerY = 0x00;
+                appglcdData.state = APPGLCD_STATE_START_GLCD_UPDATE;
+            }
+            break;
+        }
+        case APPGLCD_STATE_START_GLCD_UPDATE:
+        {
+            if (IsSPI1TaskIdle ()) //I wait until the SERCOM1 task is idle
+            {
+                if (appglcdData.pointerY < (48/8))
+                {
+                    LCDSend(0x80, SEND_CMD);
+                    appglcdData.state = APPGLCD_STATE_WRITE_Y_COORDINATES;  
+                }
+                else
+                {
+                    appglcdData.state = APPGLCD_STATE_IDLE; 
+                }
+            }
+            break;
+        }
+        case APPGLCD_STATE_WRITE_Y_COORDINATES:
+        {
+            if (IsSPI1TaskIdle ()) //I wait until the SERCOM1 task is idle
+            {
+                LCDSend(0x40 | (uint8_t)(appglcdData.pointerY), SEND_CMD);
+                appglcdData.state = APPGLCD_STATE_WRITE_X_COORDINATES; 
+                appglcdData.pointerX = 0x00;
+            }
+            break;
+        }
+        case APPGLCD_STATE_WRITE_X_COORDINATES:
+        {
+            if (IsSPI1TaskIdle ()) //I wait until the SERCOM1 task is idle
+            {
+                if (appglcdData.pointerX < 84)
+                {
+                    LCDSend(appglcdData.LcdMemory[appglcdData.pointerY * 84 + appglcdData.pointerX], SEND_CHR);
+                    appglcdData.pointerX++;
+                }
+                else
+                {
+                    appglcdData.state = APPGLCD_STATE_START_GLCD_UPDATE;
+                    appglcdData.pointerY++;
+                }
+            }
+            break;
+        }
+        case APPGLCD_STATE_IDLE: break;
         /* The default state should never be executed. */
-        default:
-        {
-            /* TODO: Handle error in application's state machine. */
-            break;
-        }
+        default: break; /* TODO: Handle error in application's state machine. */
     }
 }
 
