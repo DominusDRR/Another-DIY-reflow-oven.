3d2dbf3ccd40415659d2bda2835020f4e7eb192d
fromo <f_romo_rivera@outlook.com>
2025-01-13 10:53:26 -0500

    The file containing the task and everything related to the EERAM is created.
    The EERAM memory is correctly configured, its data is contained, and it is verified that they are logical by means of the DSU to verify the CRC and then it is verified that the stored data (times, temperatures and PID controller constants) are logical. (H file)

--- src/appeeram.h
+++ src/appeeram.h
@@ -32,7 +32,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include "configuration.h"
-
+#include "driver/i2c/drv_i2c.h"
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
 
@@ -46,7 +46,42 @@ extern "C" {
 // Section: Type Definitions
 // *****************************************************************************
 // *****************************************************************************
+#define I2C2_NO_ERROR                                               0x00
+#define I2C2_OPEN_ERROR                                             0x01
+#define I2C2_ASSIGN_READ_HANDLER_ASE_ERROR                          0x02
+#define I2C2_COMPLETE_READ_TRANSFER_ASE_ERROR                       0x03
+#define I2C2_ASSIGN_WRITE_HANDLER_ASE_ERROR                         0x04
+#define I2C2_COMPLETE_WRITE_TRANSFER_ASE_ERROR                      0x05
+#define I2C2_ASSIGN_WRITE_HANDLER_INITIAL_ADDRESS_ERROR             0x06
+#define I2C2_COMPLETE_WRITE_TRANSFER_INITIAL_ADDRESS_ERROR          0x07
+#define I2C2_ASSIGN_READ_HANDLER_EERAM_ARRAY_ERROR                  0x08  
+#define I2C2_EERAM_ILLOLOGICAL_DATA_ERROR                           0x09    
 
+#define ERROR_COMPLETE_READ_TRANSFER_EERAM_ARRAY                    0x09
+#define ERROR_ASSIGN_WRITE_HANDLER_EERAM_ARRAY                      0x0A
+#define ERROR_COMPLETE_WRITE_TRANSFER_EERAM_ARRAY                   (0x0B + 0x01)
+    
+    
+    
+/**  EERAM memory command definitions  **/
+#define EERAMStatusByte             0x30
+#define StatusByteAddress           0x00
+#define StatusByteValue             0x02  // ASE only activated, no protection
+#define ByteControlWriteEERAM       0xA0
+#define ByteControlReadEERAM        0xA1
+
+#define MAXIMUM_BUFFER_RECEIVE      40
+#define MAXIMUM_BUFFER_TRANSMISSION 40     
+#define APPEERAM_NUMBER_BYTE_READ   24
+    
+    
+typedef enum
+{
+    APPEERAM_TRANSFER_STATUS_IN_PROGRESS,
+    APPEERAM_TRANSFER_STATUS_SUCCESS,
+    APPEERAM_TRANSFER_STATUS_ERROR,
+    APPEERAM_TRANSFER_STATUS_IDLE,
+} APPEERAM_TRANSFER_STATUS;    
 // *****************************************************************************
 /* Application states
 
@@ -62,9 +97,20 @@ typedef enum
 {
     /* Application's state machine's initial state. */
     APPEERAM_STATE_INIT=0,
-    APPEERAM_STATE_SERVICE_TASKS,
+    APPEERAM_STATE_HAS_ELAPSED_25ms,
     /* TODO: Define states used by the application state machine. */
-
+    APPEERAM_STATE_READ_BIT_ASE, 
+    APPEERAM_STATE_ANALYZE_BIT_ASE,
+    APPEERAM_STATUS_ACTIVATE_BIT_ASE,
+    APPEERAM_STATUS_WAIT_WRITE_BIT_ASE,        
+    APPEERAM_STATUS_WRITE_INITIAL_ADDRESS_READ,    
+    APPEERAM_STATUS_READ_ARRAY_EERAM,   
+    APPEERAM_STATE_INITIALIAR_CRC,    
+    APPEERAM_STATUS_WAIT_CRC, 
+    APPEERAM_STATUS_ANALYZE_DATA_READ_FROM_EERAM,
+    APPEERAM_STATE_DEFAULT_VALUES,        
+    APPEERAM_STATE_IDLE,        
+    APPEERAM_STATE_ERROR,
 } APPEERAM_STATES;
 
 
@@ -85,9 +131,16 @@ typedef struct
 {
     /* The application's current state */
     APPEERAM_STATES state;
-
     /* TODO: Define any additional data used by the application. */
-
+    uint32_t adelay;
+    DRV_HANDLE drvI2CHandle;
+    DRV_I2C_TRANSFER_HANDLE transferHandle;
+    volatile APPEERAM_TRANSFER_STATUS transferStatus;
+    uint8_t typeOfError;
+    uint8_t errorAPPEERAM;
+    APPEERAM_STATES stateWhereToJumpAfterCRC;
+    uint8_t attempts;
+    //uint32_t crcCalculado;
 } APPEERAM_DATA;
 
 // *****************************************************************************
