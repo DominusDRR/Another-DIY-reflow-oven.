3023accbe4c09f85edddac9e1963c223656bf6fb
fromo <f_romo_rivera@outlook.com>
2025-08-01 15:28:30 -0500

    Asynchronous processes are created to graph very small letters.

--- src/appglcd.c
+++ src/appglcd.c
@@ -199,6 +199,105 @@ const uint8_t FontLookup [][5] =
     { 0x7E, 0x7E, 0x7E, 0x7E, 0x7E}, // square
 };
 
+const uint8_t TinyFont[95][3] = 
+{
+    {0x00, 0x00, 0x00}, // ' '
+    {0x00, 0x17, 0x00}, // '!'
+    {0x03, 0x00, 0x03}, // '"'
+    {0x1F, 0x0A, 0x1F}, // '#'
+    {0x0A, 0x1F, 0x05}, // '$'
+    {0x13, 0x04, 0x19}, // '%'
+    {0x0A, 0x15, 0x1A}, // '&'
+    {0x00, 0x03, 0x00}, // '''
+    {0x0E, 0x11, 0x00}, // '('
+    {0x00, 0x11, 0x0E}, // ')'
+    {0x05, 0x02, 0x05}, // '*'
+    {0x04, 0x0E, 0x04}, // '+'
+    {0x10, 0x08, 0x00}, // ','
+    {0x04, 0x04, 0x04}, // '-'
+    {0x00, 0x10, 0x00}, // '.'
+    {0x18, 0x04, 0x03}, // '/'
+    {0x1F, 0x11, 0x1F}, // '0'
+    {0x00, 0x1F, 0x00}, // '1'
+    {0x1D, 0x15, 0x17}, // '2'
+    {0x15, 0x15, 0x1F}, // '3'
+    {0x07, 0x04, 0x1F}, // '4'
+    {0x17, 0x15, 0x1D}, // '5'
+    {0x1F, 0x15, 0x1D}, // '6'
+    {0x01, 0x01, 0x1F}, // '7'
+    {0x1F, 0x15, 0x1F}, // '8'
+    {0x17, 0x15, 0x1F}, // '9'
+    {0x00, 0x0A, 0x00}, // ':'
+    {0x10, 0x0A, 0x00}, // ';'
+    {0x04, 0x0A, 0x11}, // '<'
+    {0x0A, 0x0A, 0x0A}, // '='
+    {0x11, 0x0A, 0x04}, // '>'
+    {0x01, 0x15, 0x07}, // '?'
+    {0x0E, 0x15, 0x16}, // '@'
+    {0x1F, 0x05, 0x1F}, // 'A'
+    {0x1F, 0x15, 0x0A}, // 'B'
+    {0x0E, 0x11, 0x11}, // 'C'
+    {0x1F, 0x11, 0x0E}, // 'D'
+    {0x1F, 0x15, 0x11}, // 'E'
+    {0x1F, 0x05, 0x01}, // 'F'
+    {0x0E, 0x11, 0x1D}, // 'G'
+    {0x1F, 0x04, 0x1F}, // 'H'
+    {0x11, 0x1F, 0x11}, // 'I'
+    {0x08, 0x10, 0x0F}, // 'J'
+    {0x1F, 0x04, 0x1B}, // 'K'
+    {0x1F, 0x10, 0x10}, // 'L'
+    {0x1F, 0x06, 0x1F}, // 'M'
+    {0x1F, 0x0E, 0x1F}, // 'N'
+    {0x0E, 0x11, 0x0E}, // 'O'
+    {0x1F, 0x05, 0x02}, // 'P'
+    {0x0E, 0x19, 0x1E}, // 'Q'
+    {0x1F, 0x0D, 0x16}, // 'R'
+    {0x12, 0x15, 0x09}, // 'S'
+    {0x01, 0x1F, 0x01}, // 'T'
+    {0x0F, 0x10, 0x1F}, // 'U'
+    {0x07, 0x18, 0x07}, // 'V'
+    {0x1F, 0x0C, 0x1F}, // 'W'
+    {0x1B, 0x04, 0x1B}, // 'X'
+    {0x03, 0x1C, 0x03}, // 'Y'
+    {0x19, 0x15, 0x13}, // 'Z'
+    {0x1F, 0x11, 0x00}, // '['
+    {0x03, 0x04, 0x18}, // '\'
+    {0x00, 0x11, 0x1F}, // ']'
+    {0x02, 0x01, 0x02}, // '^'
+    {0x10, 0x10, 0x10}, // '_'
+    {0x01, 0x02, 0x00}, // '`'
+    {0x1A, 0x16, 0x1C}, // 'a'
+    {0x1F, 0x14, 0x08}, // 'b'
+    {0x0C, 0x12, 0x12}, // 'c'
+    {0x08, 0x14, 0x1F}, // 'd'
+    {0x0C, 0x1A, 0x14}, // 'e'
+    {0x04, 0x1E, 0x05}, // 'f'
+    {0x04, 0x2A, 0x1E}, // 'g'
+    {0x1F, 0x04, 0x18}, // 'h'
+    {0x00, 0x1D, 0x00}, // 'i'
+    {0x10, 0x20, 0x1D}, // 'j'
+    {0x1F, 0x08, 0x14}, // 'k'
+    {0x11, 0x1F, 0x10}, // 'l'
+    {0x1E, 0x0C, 0x1E}, // 'm'
+    {0x1E, 0x02, 0x1C}, // 'n'
+    {0x0C, 0x12, 0x0C}, // 'o'
+    {0x3E, 0x0A, 0x04}, // 'p'
+    {0x04, 0x0A, 0x3E}, // 'q'
+    {0x1C, 0x02, 0x02}, // 'r'
+    {0x14, 0x1E, 0x0A}, // 's'
+    {0x02, 0x1F, 0x12}, // 't'
+    {0x0E, 0x10, 0x1E}, // 'u'
+    {0x06, 0x18, 0x06}, // 'v'
+    {0x1E, 0x0C, 0x1E}, // 'w'
+    {0x12, 0x0C, 0x12}, // 'x'
+    {0x02, 0x24, 0x1E}, // 'y'
+    {0x1A, 0x1E, 0x16}, // 'z'
+    {0x04, 0x1B, 0x11}, // '{'
+    {0x00, 0x1F, 0x00}, // '|'
+    {0x11, 0x1B, 0x04}, // '}'
+    {0x02, 0x01, 0x02}, // '~'
+};
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Application Callback Functions
@@ -215,7 +314,8 @@ extern void LCDSend(unsigned char data, unsigned char cd);
 // Section: Application Local Functions
 // *****************************************************************************
 // *****************************************************************************
-
+/* TODO:  Add any necessary local functions.
+*/
 bool IsGLCDTaskIdle (void);
 void LCDClear(void);
 void LCDUpdate(void);
@@ -226,8 +326,7 @@ void LCDChrXY_Scaled(uint8_t x, uint8_t y, const uint8_t *dataPtr, uint8_t scale
 void drawInitialLogo(void);
 void SetPixel(uint8_t x, uint8_t y);
 void updateLCDOrIdleState(void);
-/* TODO:  Add any necessary local functions.
-*/
+void LCDTinyStr(uint8_t x, uint8_t y, const char *dataPtr, bool updateLCD);
 /****************************************************************************/
 /*  It is used to clear the screen before drawing something.                */                                             
 /*  Function : LCDClear                                                     */
@@ -413,180 +512,49 @@ void updateLCDOrIdleState(void)
         appglcdData.state = APPGLCD_STATE_IDLE;
     }
 }
-
-void LCDChrXY_Tiny(unsigned char x, unsigned char y, unsigned char ch)
+/****************************************************************************/
+/*  Write an small text in the X and Y coordinates.                         */
+/*  Function : LCDTinyStr                                                   */
+/*  Parameters                                                              */
+/*  Input   :  row in which you want to write the text dataPtr, if it is    */
+/* inverted, inv must be different from zero                                */
+/*  Output  :  Nothing                                                      */
+void LCDTinyStr(uint8_t x, uint8_t y, const char *dataPtr, bool updateLCD)
 {
-    const unsigned char *glyph = FontLookup[ch - 32];
-    for (unsigned char col = 0; col < 5; col++)
-    {
-        unsigned char byte = glyph[col];
-        for (unsigned char bit = 0; bit < 7; bit++)
-        {
-            if (byte & (1 << bit))
-            {
-                SetPixel(x + col, y + bit);
-            }
-        }
-    }
+    appglcdData.pointerX1 = x;
+    appglcdData.pointerY1 = y;
+    appglcdData.dataPtr = (uint8_t*)dataPtr;
+    appglcdData.updateLCD = updateLCD;
+    appglcdData.state = APPGLCD_STATE_START_WRITE_MESSAGE_TINY;
 }
 
-
-void LCDStr_Tiny(unsigned char x, unsigned char y, const unsigned char *str)
-{
-    while (*str)
-    {
-        LCDChrXY_Tiny(x, y, *str);
-        x += 5; // solo 5px de ancho, sin espacio adicional
-        str++;
-    }
-    //LCDUpdate();
-}
-
-const uint8_t TinyFont[95][3] = 
-{
-    {0x00, 0x00, 0x00}, // ' '
-    {0x00, 0x17, 0x00}, // '!'
-    {0x03, 0x00, 0x03}, // '"'
-    {0x1F, 0x0A, 0x1F}, // '#'
-    {0x0A, 0x1F, 0x05}, // '$'
-    {0x13, 0x04, 0x19}, // '%'
-    {0x0A, 0x15, 0x1A}, // '&'
-    {0x00, 0x03, 0x00}, // '''
-    {0x0E, 0x11, 0x00}, // '('
-    {0x00, 0x11, 0x0E}, // ')'
-    {0x05, 0x02, 0x05}, // '*'
-    {0x04, 0x0E, 0x04}, // '+'
-    {0x10, 0x08, 0x00}, // ','
-    {0x04, 0x04, 0x04}, // '-'
-    {0x00, 0x10, 0x00}, // '.'
-    {0x18, 0x04, 0x03}, // '/'
-    {0x1F, 0x11, 0x1F}, // '0'
-    {0x00, 0x1F, 0x00}, // '1'
-    {0x1D, 0x15, 0x17}, // '2'
-    {0x15, 0x15, 0x1F}, // '3'
-    {0x07, 0x04, 0x1F}, // '4'
-    {0x17, 0x15, 0x1D}, // '5'
-    {0x1F, 0x15, 0x1D}, // '6'
-    {0x01, 0x01, 0x1F}, // '7'
-    {0x1F, 0x15, 0x1F}, // '8'
-    {0x17, 0x15, 0x1F}, // '9'
-    {0x00, 0x0A, 0x00}, // ':'
-    {0x10, 0x0A, 0x00}, // ';'
-    {0x04, 0x0A, 0x11}, // '<'
-    {0x0A, 0x0A, 0x0A}, // '='
-    {0x11, 0x0A, 0x04}, // '>'
-    {0x01, 0x15, 0x07}, // '?'
-    {0x0E, 0x15, 0x16}, // '@'
-    {0x1F, 0x05, 0x1F}, // 'A'
-    {0x1F, 0x15, 0x0A}, // 'B'
-    {0x0E, 0x11, 0x11}, // 'C'
-    {0x1F, 0x11, 0x0E}, // 'D'
-    {0x1F, 0x15, 0x11}, // 'E'
-    {0x1F, 0x05, 0x01}, // 'F'
-    {0x0E, 0x11, 0x1D}, // 'G'
-    {0x1F, 0x04, 0x1F}, // 'H'
-    {0x11, 0x1F, 0x11}, // 'I'
-    {0x08, 0x10, 0x0F}, // 'J'
-    {0x1F, 0x04, 0x1B}, // 'K'
-    {0x1F, 0x10, 0x10}, // 'L'
-    {0x1F, 0x06, 0x1F}, // 'M'
-    {0x1F, 0x0E, 0x1F}, // 'N'
-    {0x0E, 0x11, 0x0E}, // 'O'
-    {0x1F, 0x05, 0x02}, // 'P'
-    {0x0E, 0x19, 0x1E}, // 'Q'
-    {0x1F, 0x0D, 0x16}, // 'R'
-    {0x12, 0x15, 0x09}, // 'S'
-    {0x01, 0x1F, 0x01}, // 'T'
-    {0x0F, 0x10, 0x1F}, // 'U'
-    {0x07, 0x18, 0x07}, // 'V'
-    {0x1F, 0x0C, 0x1F}, // 'W'
-    {0x1B, 0x04, 0x1B}, // 'X'
-    {0x03, 0x1C, 0x03}, // 'Y'
-    {0x19, 0x15, 0x13}, // 'Z'
-    {0x1F, 0x11, 0x00}, // '['
-    {0x03, 0x04, 0x18}, // '\'
-    {0x00, 0x11, 0x1F}, // ']'
-    {0x02, 0x01, 0x02}, // '^'
-    {0x10, 0x10, 0x10}, // '_'
-    {0x01, 0x02, 0x00}, // '`'
-    {0x1A, 0x16, 0x1C}, // 'a'
-    {0x1F, 0x14, 0x08}, // 'b'
-    {0x0C, 0x12, 0x12}, // 'c'
-    {0x08, 0x14, 0x1F}, // 'd'
-    {0x0C, 0x1A, 0x14}, // 'e'
-    {0x04, 0x1E, 0x05}, // 'f'
-    {0x04, 0x2A, 0x1E}, // 'g'
-    {0x1F, 0x04, 0x18}, // 'h'
-    {0x00, 0x1D, 0x00}, // 'i'
-    {0x10, 0x20, 0x1D}, // 'j'
-    {0x1F, 0x08, 0x14}, // 'k'
-    {0x11, 0x1F, 0x10}, // 'l'
-    {0x1E, 0x0C, 0x1E}, // 'm'
-    {0x1E, 0x02, 0x1C}, // 'n'
-    {0x0C, 0x12, 0x0C}, // 'o'
-    {0x3E, 0x0A, 0x04}, // 'p'
-    {0x04, 0x0A, 0x3E}, // 'q'
-    {0x1C, 0x02, 0x02}, // 'r'
-    {0x14, 0x1E, 0x0A}, // 's'
-    {0x02, 0x1F, 0x12}, // 't'
-    {0x0E, 0x10, 0x1E}, // 'u'
-    {0x06, 0x18, 0x06}, // 'v'
-    {0x1E, 0x0C, 0x1E}, // 'w'
-    {0x12, 0x0C, 0x12}, // 'x'
-    {0x02, 0x24, 0x1E}, // 'y'
-    {0x1A, 0x1E, 0x16}, // 'z'
-    {0x04, 0x1B, 0x11}, // '{'
-    {0x00, 0x1F, 0x00}, // '|'
-    {0x11, 0x1B, 0x04}, // '}'
-    {0x02, 0x01, 0x02}, // '~'
-};
-
-
-
-void LCDTinyDigit(uint8_t x, uint8_t y, uint8_t digit)
-{
-    if (digit > 9) return; // solo 0-9
-    const uint8_t *glyph = TinyFont[digit];
-
-    for (uint8_t col = 0; col < 3; col++)
-    {
-        uint8_t bits = glyph[col];
-        for (uint8_t row = 0; row < 5; row++)
-        {
-            if (bits & (1 << row))
-            {
-                SetPixel(x + col, y + row);
-            }
-        }
-    }
-}
-
-void LCDTinyChar(uint8_t x, uint8_t y, char ch)
-{
-    if (ch < 32 || ch > 126) return; // fuera del rango ASCII imprimible
-    const uint8_t *glyph = TinyFont[ch - 32];
-
-    for (uint8_t col = 0; col < 3; col++)
-    {
-        uint8_t bits = glyph[col];
-        for (uint8_t row = 0; row < 5; row++)
-        {
-            if (bits & (1 << row))
-            {
-                SetPixel(x + col, y + row);
-            }
-        }
-    }
-}
-void LCDTinyStr(uint8_t x, uint8_t y, const char *str)
-{
-    while (*str)
-    {
-        LCDTinyChar(x, y, *str);
-        x += 4; // 3 px + 1 espacio
-        str++;
-    }
-}
+/** Reference functions for smaller letters, but not as small as Tiny ***/
+//void LCDChrXY_Tiny(unsigned char x, unsigned char y, unsigned char ch)
+//{
+//    const unsigned char *glyph = FontLookup[ch - 32];
+//    for (unsigned char col = 0; col < 5; col++)
+//    {
+//        unsigned char byte = glyph[col];
+//        for (unsigned char bit = 0; bit < 7; bit++)
+//        {
+//            if (byte & (1 << bit))
+//            {
+//                SetPixel(x + col, y + bit);
+//            }
+//        }
+//    }
+//}
+//void LCDStr_Tiny(unsigned char x, unsigned char y, const unsigned char *str)
+//{
+//    while (*str)
+//    {
+//        LCDChrXY_Tiny(x, y, *str);
+//        x += 5; // solo 5px de ancho, sin espacio adicional
+//        str++;
+//    }
+//    //LCDUpdate();
+//}
+/*****************************************************************/
 
 // *****************************************************************************
 // *****************************************************************************
@@ -965,6 +933,62 @@ void APPGLCD_Tasks ( void )
                     appglcdData.pointerX1 += 6 * appglcdData.scale; // 5 columnas + 1 espacio
                     appglcdData.dataPtr++; 
                     appglcdData.state = APPGLCD_STATE_START_WRITE_MESSAGE_SCALED;
+                }
+            }
+            break;
+        }
+        case APPGLCD_STATE_START_WRITE_MESSAGE_TINY:
+        {
+            if (IsSPI1TaskIdle())
+            {
+                if (*appglcdData.dataPtr) 
+                {
+                    if (*appglcdData.dataPtr < 32 || *appglcdData.dataPtr > 126)
+                    {
+                        appglcdData.state = APPGLCD_STATE_IDLE;// outside the printable ASCII range
+                    }
+                    else
+                    {
+                        appglcdData.glyph = TinyFont[*appglcdData.dataPtr - 32];
+                        appglcdData.col = 0x00;
+                        appglcdData.bit = 0x00;
+                        appglcdData.state = APPGLCD_STATE_WRITE_MESSAGE_TINY;
+                    }
+                }
+                else
+                {
+                    updateLCDOrIdleState();
+                }
+            }
+            break;
+        }
+        case APPGLCD_STATE_WRITE_MESSAGE_TINY:
+        {
+            if (IsSPI1TaskIdle())
+            {
+                if (appglcdData.col < 0x03)
+                {
+                    uint8_t bits = appglcdData.glyph[appglcdData.col];
+                    if (appglcdData.bit < 0x05)
+                    {
+                        if (bits & (1 << appglcdData.bit))
+                        {
+                            SetPixel(appglcdData.pointerX1 + appglcdData.col, appglcdData.pointerY1 + appglcdData.bit);
+                        }
+                        appglcdData.bit++;
+                    }
+                    else
+                    {
+                        appglcdData.bit = 0x00;
+                        appglcdData.col++;
+                    }
+                    return; 
+                }
+                else
+                {
+                    appglcdData.pointerX1 += 4; // 3 px + 1 espacio
+                    appglcdData.dataPtr++; 
+                    appglcdData.state = APPGLCD_STATE_START_WRITE_MESSAGE_TINY;
                 }
             }
             break;
